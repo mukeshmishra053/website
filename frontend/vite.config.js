dbe0a45e93c5c3fe65c637b454dab1a06e5b9706
Mukesh Mishra <53799602+mukeshmishra053@users.noreply.github.com>
2025-12-11 14:49:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    allowedHosts: ['website-vuo9.onrender.com'],
+    host: true,          // or '0.0.0.0'
+    port: 4173           // will be overridden by CLI --port $PORT
+  }
 })
